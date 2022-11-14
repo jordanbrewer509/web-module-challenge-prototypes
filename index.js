0c94fd61e1ceebbca19bab1bd8f80a83f434a543
Jordan~ <jordan.brewer509@gmail.com>
2022-11-13 18:45:53 -0800

    finished assignment

--- index.js
+++ index.js
@@ -15,9 +15,27 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+  Person.prototype.eat = function(food) {
+    if(this.stomach.length <= 10) {
+      this.stomach.push(food);
+    }
+  }    
+
+
+  Person.prototype.poop = function() {
+    this.stomach = [];
+  }
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`
+  }
+
+
+
 
 
 /*
@@ -36,10 +54,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;  
+  this.tank = 0;
+  this.odometer = 0;
 }
-
+Car.prototype.fill = function(gallons) {
+  this.tank += gallons
+}
 
 /*
   TASK 3
@@ -49,18 +72,24 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
 
+function Baby(name, age, favoriteToy) {
+this.name = name;
+this.age = age;
+this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function play() {
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. In window binding 'this' is used to refer to global variables.
+  2. In implicit binding 'this' is used to refer to object key value pairs.
+  3. In explicit binding 'this' is used to refer to objects.
+  4. In new binding 'this' is used to refer to variables within a function.
 */
 
 ///////// END OF CHALLENGE /////////
